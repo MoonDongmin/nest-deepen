27917b1f4aeb403f1553596f6ece994534b7ee28
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-06-26 11:36:09 +0900

    Mapped Types 적용하기

--- dist/genre/dto/update-genre.dto.js
+++ dist/genre/dto/update-genre.dto.js
@@ -1,23 +1,9 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UpdateGenreDto = void 0;
-const class_validator_1 = require("class-validator");
-class UpdateGenreDto {
+const mapped_types_1 = require("@nestjs/mapped-types");
+const create_genre_dto_1 = require("./create-genre.dto");
+class UpdateGenreDto extends (0, mapped_types_1.PartialType)(create_genre_dto_1.CreateGenreDto) {
 }
 exports.UpdateGenreDto = UpdateGenreDto;
-__decorate([
-    (0, class_validator_1.IsOptional)(),
-    (0, class_validator_1.IsString)(),
-    (0, class_validator_1.IsNotEmpty)(),
-    __metadata("design:type", String)
-], UpdateGenreDto.prototype, "name", void 0);
 //# sourceMappingURL=update-genre.dto.js.map
--- dist/movie/dto/update-movie.dto.d.ts
+++ dist/movie/dto/update-movie.dto.d.ts
@@ -1,6 +1,5 @@
-export declare class UpdateMovieDto {
-    title?: string;
-    genreIds?: number[];
-    detail?: string;
-    directorId?: number;
+import { CreateMovieDto } from './create-movie.dto';
+declare const UpdateMovieDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateMovieDto>>;
+export declare class UpdateMovieDto extends UpdateMovieDto_base {
 }
+export {};
