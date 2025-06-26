8e284111ea15ffb752305bf3c55af3006f520567
MoonDongmin <109586658+MoonDongmin@users.noreply.github.com>
2025-06-26 11:37:09 +0900

    #9-mapped-types
    - updateDto에 Mapped Type 적용하기

--- dist/movie/dto/update-movie.dto.js
+++ dist/movie/dto/update-movie.dto.js
@@ -1,49 +1,9 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UpdateMovieDto = void 0;
-const class_validator_1 = require("class-validator");
-var MovieGenre;
-(function (MovieGenre) {
-    MovieGenre["Fantasy"] = "fantasy";
-    MovieGenre["Action"] = "action";
-})(MovieGenre || (MovieGenre = {}));
-class UpdateMovieDto {
+const create_movie_dto_1 = require("./create-movie.dto");
+const mapped_types_1 = require("@nestjs/mapped-types");
+class UpdateMovieDto extends (0, mapped_types_1.PartialType)(create_movie_dto_1.CreateMovieDto) {
 }
 exports.UpdateMovieDto = UpdateMovieDto;
-__decorate([
-    (0, class_validator_1.IsNotEmpty)(),
-    (0, class_validator_1.IsString)(),
-    (0, class_validator_1.IsOptional)(),
-    __metadata("design:type", String)
-], UpdateMovieDto.prototype, "title", void 0);
-__decorate([
-    (0, class_validator_1.IsArray)(),
-    (0, class_validator_1.ArrayNotEmpty)(),
-    (0, class_validator_1.IsNumber)({}, {
-        each: true,
-    }),
-    (0, class_validator_1.IsOptional)(),
-    __metadata("design:type", Array)
-], UpdateMovieDto.prototype, "genreIds", void 0);
-__decorate([
-    (0, class_validator_1.IsNotEmpty)(),
-    (0, class_validator_1.IsString)(),
-    (0, class_validator_1.IsOptional)(),
-    __metadata("design:type", String)
-], UpdateMovieDto.prototype, "detail", void 0);
-__decorate([
-    (0, class_validator_1.IsNotEmpty)(),
-    (0, class_validator_1.IsNumber)(),
-    (0, class_validator_1.IsOptional)(),
-    __metadata("design:type", Number)
-], UpdateMovieDto.prototype, "directorId", void 0);
 //# sourceMappingURL=update-movie.dto.js.map
--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,16 +1,5 @@
-import {
-  ArrayNotEmpty,
-  IsArray,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-} from 'class-validator';
-
-enum MovieGenre {
-  Fantasy = 'fantasy',
-  Action = 'action',
-}
+import { CreateMovieDto } from './create-movie.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
 // Custom Validator
 // @ValidatorConstraint({
@@ -40,30 +29,4 @@ enum MovieGenre {
 //   };
 // }
 
-export class UpdateMovieDto {
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
-  title?: string;
-
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsNumber(
-    {},
-    {
-      each: true,
-    },
-  )
-  @IsOptional()
-  genreIds?: number[];
-
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
-  detail?: string;
-
-  @IsNotEmpty()
-  @IsNumber()
-  @IsOptional()
-  directorId?: number;
-}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
