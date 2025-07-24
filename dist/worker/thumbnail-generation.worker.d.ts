50386c1316dfd5701735afa11ac30f724e7f3f4c
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-07-24 11:22:25 +0900

    썸네일 작업하기

--- dist/worker/thumbnail-generation.worker.d.ts
+++ dist/worker/thumbnail-generation.worker.d.ts
@@ -1,5 +1,5 @@
 import { WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 export declare class ThumbnailGenerationProcess extends WorkerHost {
-    process(job: Job, token?: string): Promise<number>;
+    process(job: Job, token?: string): Promise<any>;
 }
--- dist/worker/thumbnail-generation.worker.js
+++ dist/worker/thumbnail-generation.worker.js
@@ -8,10 +8,28 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ThumbnailGenerationProcess = void 0;
 const bullmq_1 = require("@nestjs/bullmq");
+const path_1 = require("path");
+const process_1 = require("process");
+const ffmpegFluent = require("fluent-ffmpeg");
 let ThumbnailGenerationProcess = class ThumbnailGenerationProcess extends bullmq_1.WorkerHost {
     async process(job, token) {
         const { videoPath, videoId } = job.data;
-        console.log(job.data);
+        console.log(`영상 트랜스코딩중... ID: ${videoId}`);
+        const outputDirectory = (0, path_1.join)((0, process_1.cwd)(), 'public', 'thumbnail');
+        ffmpegFluent(videoPath)
+            .screenshots({
+            count: 1,
+            filename: `${videoId}.png`,
+            folder: outputDirectory,
+            size: '320x240',
+        })
+            .on('end', () => {
+            console.log(`썸내일 생성 완료! ID: ${videoId}`);
+        })
+            .on('error', (error) => {
+            console.log(error);
+            console.log(`썸네일 생성 실패! ID: ${videoId}`);
+        });
         return 0;
     }
 };
